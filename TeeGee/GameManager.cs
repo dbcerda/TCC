cc4d7e0d8e4f4a38caa70282f2eec3b2667067b8
dbcerda <dbcerda@inf.ufsm.br>
2014-11-27 16:36:24 -0200

    Adicionados sprites dos personagens

---
+++ TeeGee/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace TeeGee
+{
+    class GameManager
+    {        
+        private Rectangle viewArea;
+        private Character characterA;
+        private Character characterB;
+
+        public GameManager()
+        {
+
+        }
+
+        private void Draw()
+        {
+
+        }
+    }
+}
